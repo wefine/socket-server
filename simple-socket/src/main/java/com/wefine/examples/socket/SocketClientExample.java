95b1c3c42873434e2a4a7b60035a3a426e6158d8
wefine <wefine@aliyun.com>
2017-05-09 20:04:59 +0800

    init
    Signed-off-by: wefine <wefine@aliyun.com>

---
+++ simple-socket/src/main/java/com/wefine/examples/socket/SocketClientExample.java
@@ -0,0 +1,33 @@
+package com.wefine.examples.socket;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class SocketClientExample {
+
+    public void startClient()
+            throws IOException, InterruptedException {
+
+        InetSocketAddress hostAddress = new InetSocketAddress("localhost", 8090);
+        SocketChannel client = SocketChannel.open(hostAddress);
+
+        System.out.println("Client... started");
+        String threadName = Thread.currentThread().getName();
+
+        // Send messages to server
+        String[] messages = new String[]
+                {threadName + ": test1", threadName + ": test2", threadName + ": test3"};
+
+        for (String m : messages) {
+            byte[] message = m.getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(message);
+            client.write(buffer);
+            System.out.println(m);
+            buffer.clear();
+            Thread.sleep(5000);
+        }
+        client.close();
+    }
+}
